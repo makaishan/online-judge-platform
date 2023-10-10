5bdde8a5367266889d34217ee5e44f15150140ea
makaishan <1516001317@qq.com>
2023-10-10 09:11:18 +0800

    文档/项目开发计划书

--- luooj-backend-microservice/luooj-backend-question-service/src/main/java/com/mks/luoojbackendquestionservice/service/impl/QuestionSubmitServiceImpl.java
+++ luooj-backend-microservice/luooj-backend-question-service/src/main/java/com/mks/luoojbackendquestionservice/service/impl/QuestionSubmitServiceImpl.java
@@ -52,7 +52,7 @@ public class QuestionSubmitServiceImpl extends ServiceImpl<QuestionSubmitMapper,
     @Override
     public long doQuestionSubmit(QuestionSubmitAddRequest questionSubmitAddRequest, User loginUser) {
         // 判断实体是否存在，根据类别获取实体
-        Long questionId = questionSubmitAddRequest.getQuestionId();
+         Long questionId = questionSubmitAddRequest.getQuestionId();
         Question question = questionService.getById(questionId);
         if (question == null) {
             throw new BusinessException(ErrorCode.NOT_FOUND_ERROR);
