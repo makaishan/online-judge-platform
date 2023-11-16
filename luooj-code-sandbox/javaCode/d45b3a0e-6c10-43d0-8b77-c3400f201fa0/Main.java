5ac227c77ce1ccf8043179db76f9e8fabb1d9434
makaishan <1516001317@qq.com>
2023-11-16 17:12:04 +0800

    11/16 增加运行cpp代码

--- luooj-code-sandbox/javaCode/d45b3a0e-6c10-43d0-8b77-c3400f201fa0/Main.java
+++
@@ -1,7 +0,0 @@
-import java.util.*;
-public class Main {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        ystem.out.println(in.nextInt() + in.nextInt());
-    }
-}
